d0351515af848a5a0e6a040068a81d169e64988a
AnastasiaZvereva5 <zipz.ee@mail.ru>
2023-06-05 08:42:44 +0300

    commit

---
+++ Kladovshik.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tren1
+{
+    /// <summary>
+    /// Логика взаимодействия для Kladovshik.xaml
+    /// </summary>
+    public partial class Kladovshik : Window
+    {
+        public Kladovshik()
+        {
+            InitializeComponent();
+        }
+        private void Obratno(object sender, RoutedEventArgs e)
+        {
+            MainWindow MW = new MainWindow();
+            MW.Show();
+            Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using System.Windows;
+using tren1.Model;
+
+namespace tren1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Vhod_Click(object sender, RoutedEventArgs e)
+        {
+            var db = new ekzEntities();
+            var User = db.Users.AsNoTracking().FirstOrDefault(u => u.Login == LogBox.Text && u.Password == PassBox.Text);
+            if (User != null) 
+            {
+                if (User.Role == 1)
+                {
+                    Manager MW = new Manager();
+                    MW.Show();
+                    Close();
+                }
+                if (User.Role == 2)
+                {
+                    Kladovshik MW = new Kladovshik();
+                    MW.Show();
+                    Close();
+                }
+                if (User.Role == 3)
+                {
+                    Director MW = new Director();
+                    MW.Show();
+                    Close();
+                }
+                if (User.Role == 4)
+                {
+                    Postavka MW = new Postavka();
+                    MW.Show();
+                    Close();
+                }
+            }
+                else
+                {
+                MessageBox.Show("логин либо пароль не верны");
+                 }
+                
+            }
+
+                    private void Vihod_Click(object sender, RoutedEventArgs e)
+            {
+                Close();
+            }
+        }
+    }
+
